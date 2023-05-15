471d1e8d3364fd9cdb8d9a94e09f1805c401eb7a
Odysseas.eth <odyslam@gmail.com>
2023-05-15 21:40:59 +0200

    fix: dynamically pick ircd container's ip

--- tor/run.sh
+++ tor/run.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env sh 
-tor
+# Tor seems not to be able to reason about the docker-based dns networking
+# so we need to get the ip of the ircd container and use that instead
+IP=$(getent hosts ircd | cut -d' ' -f1)
+read -r -d '' CONFIG << EOM
+HiddenServiceDir /var/lib/tor/ircd/
+HiddenServicePort 25551 ${IP}:25551
+EOM
+echo "$CONFIG" > /etc/tor/torrc
+exec tor
